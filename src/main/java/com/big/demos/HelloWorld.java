43e3461cefe0c2e3a730ff56b4e6f227213edb2e
Sreee999 <edify99sree@gmail.com>
2025-05-26 21:35:04 +0530

    errors fixed

--- src/main/java/com/big/demos/HelloWorld.java
+++ src/main/java/com/big/demos/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World Maven Demo exec plugin*********Hai222******");
+		System.out.println("Hello World Maven Demo exec plugin*********Hai2223333******");
 
 	}
 
