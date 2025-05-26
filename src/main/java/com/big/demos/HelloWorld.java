ab232e834ab8cec7b19cc8b8741758e673d8a6fa
Sreee999 <edify99sree@gmail.com>
2025-05-26 21:26:32 +0530

    first commit

---
+++ src/main/java/com/big/demos/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.big.demos;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World Maven Demo exec plugin*********Hai222******");
+
+	}
+
+}
